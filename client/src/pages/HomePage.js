46a7e2d79428630d2867a0da50669c8ff806bd8b
ybrayevkz <96937794+ybrayevkz@users.noreply.github.com>
2022-01-22 14:15:52 +0600

    initial

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navbar} from "../components/Navbar";
+import {Helmet} from "react-helmet";
+
+
+export const HomePage = () => {
+    return(
+        <Navbar />
+    )
+}
